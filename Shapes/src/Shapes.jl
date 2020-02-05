71869d1a3f363845fc0295dee93762f5cb6d98bf
ebennewitz <60106463+ebennewitz@users.noreply.github.com>
2020-02-04 22:00:05 -0500

    Add files via upload
    Uploaded Julia package Shapes to github repository

---
+++ Shapes/src/Shapes.jl
@@ -0,0 +1,32 @@
+module Shapes
+
+# #=========================================================#
+# This package computes the area and perimeter of
+# a regular polygon. It does so with a object structure
+# defined by RegPolygon. Since a regular polygon is
+# defined to have equal side lengths, this object has
+# two components: number of sides and the side length.
+# We restrict the number of sides to be an Int64 while
+# the side length can be any number. The functions Perimeter
+# and Area compute the area and Perimeter of the RegPolygon.
+# #=========================================================#
+
+export RegPolygon
+struct RegPolygon{T1<:Integer, T2 <: Real}
+    sides::T1   #sides must be an integer
+    length::T2  #legnth can be any real number
+end
+
+export Perimeter
+function Perimeter(s::RegPolygon{<:Integer, <:Real})
+    s.sides * s.length
+end
+
+export Area
+function Area(s::RegPolygon{<:Integer, <:Real})
+    p = Perimeter(s)                        #perimeter of shape
+    a = s.length / (2 * tan(pi/s.sides))    #apothem of shape
+    p*a/2                                   #Area of n-sided polygon
+end
+
+end # module
---
+++ Shapes/test/runtests.jl
@@ -0,0 +1,25 @@
+using Shapes
+using Test
+#======================================================#
+# Runtests.jl checks that the calculated area and perimeter
+# of a regular polygon is correct. We check the cases
+# of a triangle, square and hexagon
+#======================================================#
+
+#global tolerance
+tol = 1e-13
+
+#triangle test
+tri = RegPolygon(3,1)
+@test Perimeter(tri) == tri.sides*tri.length
+@test isapprox( Area(tri) , tri.length/2 * sqrt(tri.length^2 - (0.5 * tri.length)^2), atol = tol )
+
+#square test
+sq = RegPolygon(4,1)
+@test Perimeter(sq) == sq.sides*sq.length
+@test isapprox( Area(sq) , sq.length^2, atol = tol )
+#
+#hexagon test
+hex = RegPolygon(6,1)
+@test Perimeter(hex) == 6*hex.length
+@test isapprox( Area(hex) , 3 * sqrt(3) / 2 * hex.length^2, atol = tol )
